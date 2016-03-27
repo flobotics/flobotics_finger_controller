4fa43a20d6ec9698cacf86c88c1ca118fdcda8d9
ros <info@flobotics-robotics.com>
2016-03-27 21:20:10 +0200

    modified to work with Int16MultiArray and Float32MultiArray

--- src/flobotics_finger_control_node.cpp
+++ src/flobotics_finger_control_node.cpp
@@ -1,6 +1,6 @@
 #include "ros/ros.h"
 #include <std_msgs/Int8.h>
-#include "serial/serial.h"
+//#include "serial/serial.h"
 #include <flobotics_finger_messages/flobotics_finger_force_adc_values.h>
 #include <flobotics_finger_messages/flobotics_finger_force_limit_values.h>
 #include <flobotics_finger_messages/flobotics_finger_control_values.h>
@@ -8,22 +8,29 @@
 
 #include <flobotics_finger_control_node.h>
 
+#include <std_msgs/MultiArrayLayout.h>
+#include <std_msgs/MultiArrayDimension.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Int16MultiArray.h>
 
+int val_windup = 450;
+int val_wind   = 350;
+int val_windstop = 400;  
 
 /**
  * receives force values from sensors
  */
-void flobotics_finger_force_callback(const flobotics_finger_messages::flobotics_finger_force_adc_values& msg)
+void flobotics_finger_force_callback(const std_msgs::Float32MultiArray& msg)
 {
   //ROS_INFO("I heard adc0: [%d], adc1: [%d], adc2: [%d], adc3: [%d], adc4: [%d], adc5: [%d], adc6: [%d], adc7: [%d]", msg.adc0, msg.adc1, msg.adc2, msg.adc3, msg.adc4, msg.adc5, msg.adc6, msg.adc7);
-  adc0 = msg.adc0;
-  adc1 = msg.adc1;
-  adc2 = msg.adc2;
-  adc3 = msg.adc3;
-  adc4 = msg.adc4;
-  adc5 = msg.adc5;
-  adc6 = msg.adc6;
-  adc7 = msg.adc7;
+  adc0 = (1100 / 5) * msg.data[0];
+  adc1 = (1100 / 5) * msg.data[1];
+  adc2 = (1100 / 5) * msg.data[2];
+  adc3 = (1100 / 5) * msg.data[3];
+  adc4 = (1100 / 5) * msg.data[4];
+  adc5 = (1100 / 5) * msg.data[5];
+  adc6 = (1100 / 5) * msg.data[6];
+  adc7 = (1100 / 5) * msg.data[7];
 }
 
 /*
@@ -71,7 +78,7 @@ void flobotics_finger_force_limit_values_callback(const flobotics_finger_message
  */
 void flobotics_finger_control_callback(const flobotics_finger_messages::flobotics_finger_control_values& msg)
 {
-  ROS_INFO("finger control bool:[%d]\n", msg.mode);
+  ROS_INFO("finger control mode :[%d]\n", msg.mode);
 
   mode = msg.mode;
 }
@@ -86,11 +93,12 @@ int main(int argc, char **argv)
   ros::NodeHandle nh_sub_to_force_limits;
 
 
-  ros::Subscriber sub = nh_sub_force.subscribe("adc", 1000, flobotics_finger_force_callback);
+  ros::Subscriber sub = nh_sub_force.subscribe("adc_pi_plus_sub", 1000, flobotics_finger_force_callback);
   ros::Subscriber subToControl = nh_sub_to_finger_control.subscribe("flobotics_finger_force_limit_values", 1000, flobotics_finger_force_limit_values_callback);
   ros::Subscriber subToLimits = nh_sub_to_force_limits.subscribe("flobotics_finger_control", 1000, flobotics_finger_control_callback);
 
-  servo_pub = nh_pub_to_servos.advertise<flobotics_finger_messages::flobotics_finger_servo_control_values>("pololu_usc01a", 1000);
+  //servo_pub = nh_pub_to_servos.advertise<flobotics_finger_messages::flobotics_finger_servo_control_values>("pololu_usc01a", 1000);
+  servo_pub = nh_pub_to_servos.advertise<std_msgs::Int16MultiArray>("servo_pwm_pi_sub", 100);
 
   ros::Rate loop_rate(20); //20 hertz
 
@@ -154,241 +162,204 @@ int main(int argc, char **argv)
 
 void start_drawback(){
 
+  std_msgs::Int16MultiArray msg;
+  msg.data.clear();
+
   if(adc0 < proximal_limit_min_1)
-    msg.servo0 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc0 > proximal_limit_max_1)
-    msg.servo0 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo0 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc1 < proximal_limit_min_2)
-    msg.servo1 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc1 > proximal_limit_max_2)
-    msg.servo1 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo1 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc2 < proximal_1_limit_min_1)
-    msg.servo2 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc2 > proximal_1_limit_max_1)
-    msg.servo2 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo2 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc3 < proximal_1_limit_min_2)
-    msg.servo3 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc3 > proximal_1_limit_max_2)
-    msg.servo3 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo3 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc4 < intermediate_limit_min_1)
-    msg.servo4 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc4 > intermediate_limit_max_1)
-    msg.servo4 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo4 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
 
   if(adc5 < intermediate_limit_min_2)
-    msg.servo5 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc5 > intermediate_limit_max_2)
-    msg.servo5 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo5 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc6 < distal_limit_min_1)
-    msg.servo6 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc6 > distal_limit_max_1)
-    msg.servo6 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo6 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc7 < distal_limit_min_2)
-    msg.servo7 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc7 > distal_limit_max_2)
-    msg.servo7 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo7 = 64; //stand still
-
-  msg.servo8 = 64;
-  msg.servo9 = 64;
-  msg.servo10 = 64;
-  msg.servo11 = 64;
-  msg.servo12 = 64;
-  msg.servo13 = 64;
-  msg.servo14 = 64;
-  msg.servo15 = 64;
+    msg.data.push_back(val_windstop); //stand still
 
 
 
-
-  if(cmp0 != msg.servo0 || cmp1 != msg.servo1 || cmp2 != msg.servo2 || cmp3 != msg.servo3 || cmp4 != msg.servo4 || cmp5 != msg.servo5 || cmp6 != msg.servo6 || cmp7 != msg.servo7){
-    cmp0=msg.servo0;
-    cmp1=msg.servo1;
-    cmp2=msg.servo2;
-    cmp3=msg.servo3;
-    cmp4=msg.servo4;
-    cmp5=msg.servo5;
-    cmp6=msg.servo6;
-    cmp7=msg.servo7;
-
-
-    servo_pub.publish(msg);
-  }
+  servo_pub.publish(msg);
 }
 
 void stop_drawback(){
-  msg.servo0 = 64;
-  msg.servo1 = 64;
-  msg.servo2 = 64;
-  msg.servo3 = 64;
-  msg.servo4 = 64;
-  msg.servo5 = 64;
-  msg.servo6 = 64;
-  msg.servo7 = 64;
-  msg.servo8 = 64;
-  msg.servo9 = 64;
-  msg.servo10 = 64;
-  msg.servo11 = 64;
-  msg.servo12 = 64;
-  msg.servo13 = 64;
-  msg.servo14 = 64;
-  msg.servo15 = 64;
+	std_msgs::Int16MultiArray msg;
+	msg.data.clear();
+
+	msg.data.push_back(val_windstop);
+	msg.data.push_back(val_windstop);
+	msg.data.push_back(val_windstop);
+	msg.data.push_back(val_windstop);
+	msg.data.push_back(val_windstop);
+	msg.data.push_back(val_windstop);
+	msg.data.push_back(val_windstop);
+	msg.data.push_back(val_windstop);
 
   servo_pub.publish(msg);
 
 }
 
 void distal_drawback(){
+
+	std_msgs::Int16MultiArray msg;
+	msg.data.clear();
+
   if(adc0 < proximal_limit_min_1)
-    msg.servo0 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc0 > proximal_limit_max_1)
-    msg.servo0 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo0 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc1 < proximal_limit_min_2)
-    msg.servo1 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc1 > proximal_limit_max_2)
-    msg.servo1 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo1 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
-  msg.servo2 = 64;
-  msg.servo3 = 64;
-  msg.servo4 = 64;
-  msg.servo5 = 64;
-  msg.servo6 = 64;
-  msg.servo7 = 64;
-  msg.servo8 = 64;
-  msg.servo9 = 64;
-  msg.servo10 = 64;
-  msg.servo11 = 64;
-  msg.servo12 = 64;
-  msg.servo13 = 64;
-  msg.servo14 = 64;
-  msg.servo15 = 64;
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
 
   servo_pub.publish(msg);
 }
 
 void intermediate_drawback(){
+  
+  std_msgs::Int16MultiArray msg;
+  msg.data.clear();
+
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+
   if(adc2 < proximal_1_limit_min_1)
-    msg.servo2 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc2 > proximal_1_limit_max_1)
-    msg.servo2 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo2 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc3 < proximal_1_limit_min_2)
-    msg.servo3 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc3 > proximal_1_limit_max_2)
-    msg.servo3 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo3 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
-  msg.servo0 = 64;
-  msg.servo1 = 64;
-  msg.servo4 = 64;
-  msg.servo5 = 64;
-  msg.servo6 = 64;
-  msg.servo7 = 64;
-  msg.servo8 = 64;
-  msg.servo9 = 64;
-  msg.servo10 = 64;
-  msg.servo11 = 64;
-  msg.servo12 = 64;
-  msg.servo13 = 64;
-  msg.servo14 = 64;
-  msg.servo15 = 64;
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
 
   servo_pub.publish(msg);
 }
 
 void proximal_2_drawback(){
-  if(adc4 < intermediate_limit_min_1)
-      msg.servo4 = 50; //do winding
+  
+  std_msgs::Int16MultiArray msg;
+  msg.data.clear();
+
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+
+    if(adc4 < intermediate_limit_min_1)
+      msg.data.push_back(val_wind); //do winding
     else if(adc4 > intermediate_limit_max_1)
-      msg.servo4 = 75; //winding up
+      msg.data.push_back(val_windup); //winding up
     else
-      msg.servo4 = 64; //stand still
+      msg.data.push_back(val_windstop); //stand still
 
 
     if(adc5 < intermediate_limit_min_2)
-      msg.servo5 = 50; //do winding
+      msg.data.push_back(val_wind); //do winding
     else if(adc5 > intermediate_limit_max_2)
-      msg.servo5 = 75; //winding up
+      msg.data.push_back(val_windup); //winding up
     else
-      msg.servo5 = 64; //stand still
+      msg.data.push_back(val_windstop); //stand still
 
-    msg.servo0 = 64;
-    msg.servo1 = 64;
-    msg.servo2 = 64;
-    msg.servo3 = 64;
-    msg.servo6 = 64;
-    msg.servo7 = 64;
-    msg.servo8 = 64;
-    msg.servo9 = 64;
-    msg.servo10 = 64;
-    msg.servo11 = 64;
-    msg.servo12 = 64;
-    msg.servo13 = 64;
-    msg.servo14 = 64;
-    msg.servo15 = 64;
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
 
-    servo_pub.publish(msg);
+
+  servo_pub.publish(msg);
 }
 
 void proximal_1_drawback(){
+  
+  std_msgs::Int16MultiArray msg;
+  msg.data.clear();
+
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+  msg.data.push_back(val_windstop);
+
   if(adc6 < distal_limit_min_1)
-    msg.servo6 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc6 > distal_limit_max_1)
-    msg.servo6 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo6 = 64; //stand still
+    msg.data.push_back(val_windstop); //stand still
 
   if(adc7 < distal_limit_min_2)
-    msg.servo7 = 50; //do winding
+    msg.data.push_back(val_wind); //do winding
   else if(adc7 > distal_limit_max_2)
-    msg.servo7 = 75; //winding up
+    msg.data.push_back(val_windup); //winding up
   else
-    msg.servo7 = 64; //stand still
-
-  msg.servo0 = 64;
-  msg.servo1 = 64;
-  msg.servo2 = 64;
-  msg.servo3 = 64;
-  msg.servo4 = 64;
-  msg.servo5 = 64;
-  msg.servo8 = 64;
-  msg.servo9 = 64;
-  msg.servo10 = 64;
-  msg.servo11 = 64;
-  msg.servo12 = 64;
-  msg.servo13 = 64;
-  msg.servo14 = 64;
-  msg.servo15 = 64;
+    msg.data.push_back(val_windstop); //stand still
 
   servo_pub.publish(msg);
 }
